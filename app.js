c9f8f962da42a243a116d4bbe244f2d0f9cd990a
NightWing1998 <dhruvil.s@somaiya.edu>
2019-10-19 22:30:28 +0530

    twilio done

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express=require("express");
-
-
 const app=express();
+const multer = require("multer");
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
-app.get("/test",()=>{
-   res.send("Working");
-});
 
-app.listen(3000);
+app.use(express.static(__dirname + "/public/"));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(multer({dest : "./uploads"}).any());
+  
+app.post('/twilio/callback', (req,res) => {
+	const twiml = new VoiceResponse();
+
+	// twiml.dial('8850 392 965');
+
+	const dial = twiml.dial({
+		callerId: '+918850285032'
+	});
+	dial.number('+918850285032');
+
+	// twiml.say("Good bye!!");
+
+    res.writeHead(201, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+})
+
+app.listen(3000,(err)=>err?console.error(err):null);
